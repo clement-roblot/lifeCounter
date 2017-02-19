6fbf33a9f8cff8c28afb3f011e40dea5b1aa6fc8
karlito139 <karlito@martobre.fr>
2017-02-19 22:05:54 +0100

    Added a date picker.

--- lifeCounter/settings.py
+++ lifeCounter/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    
     'counter',
 )
 
@@ -101,6 +101,7 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
+# https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
