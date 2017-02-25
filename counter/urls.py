e67694a32921d10fb4c2f392f3bcf54023061c0c
karlito139 <karlito@martobre.fr>
2017-02-25 15:16:24 +0100

    Cleaning in the files.

--- counter/urls.py
+++ counter/urls.py
@@ -9,12 +9,6 @@ urlpatterns = [
     url(r'^logIn', views.logIn, name='logIn'),
     url(r'^signOut', views.logUserOut, name='logUserOut'),
 
-
-    #url('^resetPassword/$', auth_views.password_reset),
-    #url('^resetPasswordDone/$', auth_views.password_reset_done),
-
-    #url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
-    #url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
     url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 ]
