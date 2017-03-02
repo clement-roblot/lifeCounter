5343f02ebfecb989eccfa6aa8f62f58f0d408882
karlito139 <karlito@martobre.fr>
2017-03-02 13:58:38 +0100

    Better display of the user's life in the admin page.

--- counter/admin.py
+++ counter/admin.py
@@ -2,5 +2,24 @@ from django.contrib import admin
 
 from .models import Life, Preferences
 
-admin.site.register(Life)
-admin.site.register(Preferences)
+
+
+
+class LifeAdmin(admin.ModelAdmin):
+	list_display = ['get_user']
+
+	def get_user(self, obj):
+		return obj.user.username
+
+	get_user.short_description = 'Username'
+
+admin.site.register(Life, LifeAdmin)
+
+
+
+
+class PreferencesAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Preferences, PreferencesAdmin)
+
+
